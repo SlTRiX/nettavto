ba6c212941976edab762871782bf1c75add1686e
Sitrix <116294310+SlTRiX@users.noreply.github.com>
2024-05-19 19:55:46 +0500

    Add files via upload

---
+++ front/app/page.js
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import Cars from './cars.js'
+
+export default function Home() {
+  return (
+<div className=" w-full mx-auto bg-white z-10">
+  <Cars></Cars>
+</div>
+  )
+}
